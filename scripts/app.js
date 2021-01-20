29be24657e768a28cc848a200d98d03dc281d4c9
Katrina Hicks <Katrinarhicks@gmail.com>
2021-01-20 17:58:42 -0500

    added class function in js files

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,14 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+console.log(forecast);
 
 
 const updateUI = (data) => {
 
-    // const cityDets = data.cityDets;
-    // const weather = data.weather;
-
+    
     //detructure properties - can be used instead of code above
     const { cityDets, weather } = data;
 
@@ -28,46 +29,35 @@ const updateUI = (data) => {
     icon.setAttribute('src', iconSrc);
   
 
-    let timeSrc = null;
-    if(weather.IsDayTime){
-        timeSrc = 'img/day.svg';
-    }else {
-        timeSrc = 'img/night.svg';
-    }
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
     time.setAttribute('src', timeSrc);
 
-    // remove the d-none class if present
+   // remove the d-none class if present
     if(card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
 };
 
-const updateCity = async (city) =>{
-   const cityDets = await getCity(city);
-   const weather = await getWeather(cityDets.Key);
-
-   return { cityDets, weather };
-};
-
-cityForm.addEventListener('submit', e => {
-    //prevent default action
+    cityForm.addEventListener('submit', e => {
+//     //prevent default action
     e.preventDefault();
 
-    // get city value
+//     // get city value
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-   // update the ui with new city
-  updateCity(city)
+//    // update the ui with new city
+  forecast.updateCity(city)
   .then(data => updateUI(data))
   .catch(err => console.log(err));
 
+ 
   //set local storage
   localStorage.setItem('city', city);
 });
 
 if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
        .then(data => updateUI(data))
        .catch(err => console.log(err));
 }
